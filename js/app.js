ea32fc62747e135ffe96f6214526399a17eeba2a
Giulio Zausa <mail.zausa.giulio@gmail.com>
2016-02-23 08:27:23 +0100

    Update app.js

--- js/app.js
+++ js/app.js
@@ -5,27 +5,15 @@
 
 var app = 
 {
-	onElemChange: function()
-	{
-		this.fx = parseFloat(this.elementFx.value);
-		this.fy = parseFloat(this.elementFy.value);
-		this.speed = parseFloat(this.elementSpeed.value);
-	},
-	
 	init: function()
 	{
 		this.canvas = document.getElementById("drawingCanvas"),
 		this.ctx = this.canvas.getContext("2d"),
 		
-		this.elementFx = document.getElementById("Fx"),
-		this.elementFy = document.getElementById("Fy"),
-		this.elementSpeed = document.getElementById("Speed"),
+		this.fx = 1,
+		this.fy = 1,
+		this.speed = 5,
 		
-		this.fx = parseFloat(this.elementFx.value),
-		this.fy = parseFloat(this.elementFy.value),
-		this.speed = parseFloat(this.elementSpeed.value),
-		
-		this.onElemChange();
 		startAnimation();
 	}
 }
