1da8d9df04e871e9a768d70cfc92ba8043ac1d47
giusco19 <giuliascocco21@gmail.com>
2016-03-01 14:19:24 +0100

    Update lissajous.js

--- js/lissajous.js
+++ js/lissajous.js
@@ -5,7 +5,7 @@
  
 function lissajous(t, p)
 {
-  var x = 250 * Math.cos(app.fx*t + p) + 250;
-  var y = 250 - 250 * Math.sin(app.fy*t);
+  var x = 250 * Math.cos(app.fx * t + p) + 250;
+  var y = 250 - (250 * Math.sin(app.fy * t));
   return { x, y };
 }
