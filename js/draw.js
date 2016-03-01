5416441c7a8bf339e00c58ba679ebe0bc3e63e3b
Apache98 <nicola.marigo98@gmail.com>
2016-03-01 08:57:52 +0100

    Added files via upload

--- js/draw.js
+++ js/draw.js
@@ -8,5 +8,24 @@
 
 function startAnimation()
 {
-	
+	app.ctx.lineWidth = 2;
+	app.ctx.strokeStyle = "blue";
+	var p = 0.0;
+	while (true) {
+		frame (p);
+		p += app.speed;
+	}
+}
+
+function frame(p)
+{
+	app.ctx.clearRectangle(0, 0, 512, 512);
+	app.ctx.beginPath();
+	var coord0 = lissajous(0, p);
+	app.ctx.moveTo(coord0.x, coord0.y);
+	for (t=0; t<2*Math.pi; t += 0.05) {
+		var coord = lissajous(t, p);
+		app.ctx.lineTo(coord.x, coord.y);
+	}
+	app.ctx.stroke();
 }
