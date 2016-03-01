d3840c36333605777715dbe815f5cf688f2c9870
giusco19 <giuliascocco21@gmail.com>
2016-03-01 14:15:02 +0100

    fatta la funzione lissajous

--- js/lissajous.js
+++ js/lissajous.js
@@ -2,3 +2,10 @@
  *	Funzioni per il calcolo di una curva di Lissajous dati i parametri.
  *	Giulia Scocco
  */
+ 
+function lissajous(t, p)
+{
+  var x = 250 * Math.cos(app.fx*t + p) + 250;
+  var y = 250 - 250 * Math.sin(app.fy*t);
+  return { x, y };
+}
